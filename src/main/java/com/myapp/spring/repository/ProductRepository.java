a674d71145a7405b7990bda0edc17b33810f4981
GMX\0013MR744 <deepdshah07@gmail.com>
2021-05-10 13:01:37 +0530

    DP-2 test commit-2

--- src/main/java/com/myapp/spring/repository/ProductRepository.java
+++ src/main/java/com/myapp/spring/repository/ProductRepository.java
@@ -24,7 +24,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
     
     Optional<List<Product>> findByPriceIn(Collection<Double> prices);
     
-    
     Optional<List<Product>> findByProductNameIgnoreCase(String productName);
 
 }
